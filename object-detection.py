21da7416a1a14b1224dd60cab588d358617c2f86
yut-kt <yut.kt59@gmail.com>
2018-05-23 00:21:47 +0900

    refactoring

--- object-detection.py
+++ object-detection.py
@@ -4,6 +4,9 @@ import sys
 import dlib
 import cv2
 
+RectangleColor = (0, 0, 255)  # red
+Thickness = 2  # 矩形の太さ（px）
+
 
 def main():
     # 読みこむ画像
@@ -12,14 +15,22 @@ def main():
 
     # 検出
     detector = dlib.simple_object_detector("train/detector.svm")
-    dets = detector(img)
+    detected_imgs = detector(img)
 
     # 四角形描写
-    print(str(len(dets)))
-    for d in dets:
-        cv2.rectangle(out, (d.left(), d.top()), (d.right(), d.bottom()), (0, 0, 255), 2)
+    for detected_img in detected_imgs:
+        top, left, right, bottom = get_four_corners(detected_img)
+        cv2.rectangle(out, (left, top), (right, bottom), RectangleColor, Thickness)
 
     cv2.imwrite("result.png", out)
+
+
+def get_four_corners(detected_img):
+    top = detected_img.top()
+    left = detected_img.left()
+    right = detected_img.right()
+    bottom = detected_img.bottom()
+    return top, left, right, bottom
 
 
 if __name__ == '__main__':
--- train.py
+++ train.py
@@ -1,13 +1,30 @@
 import dlib
 
-if __name__ == "__main__":
+TrainingXml = 'train/train.xml'
+SvmFile = 'train/detector.svm'
 
-    TrainingXml = "train/train.xml"
-    SvmFile = "train/detector.svm"
+
+def main():
+    dlib.train_simple_object_detector(TrainingXml, SvmFile, get_options())
+
+
+def get_options():
     options = dlib.simple_object_detector_training_options()
+
+    # True -> 左右対象画像とみなし、左右反転させた画像を学習データとして増やす
     options.add_left_right_image_flips = True
-    options.C = 5
-    options.num_threads = 4
+
+    # True -> 学習中に情報を表示する
     options.be_verbose = True
 
-    dlib.train_simple_object_detector(TrainingXml, SvmFile, options)
+    # 学習強度 大きくしすぎると過学習になるため注意
+    options.C = 5
+
+    # 並列実行数（CPUの数に設定すると最速）
+    options.num_threads = 4
+
+    return options
+
+
+if __name__ == '__main__':
+    main()
